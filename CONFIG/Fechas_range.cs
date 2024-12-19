c650d2cd6525f31dbf42a39b466e5a5333e03cf2
CRISTHIAN MELENDEZ <mele2711@gmail.com>
2024-12-19 13:09:17 -0500

    12

---
+++ CONFIG/Fechas_range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROBOTPRUEBA_V1.CONFIG
+{
+    internal class Fechas_range
+    {
+        public static void SetFechas()
+        {
+			DateTime today = DateTime.Today;
+			int daysSinceSunday = (int)today.DayOfWeek;
+			DateTime lastSundayOfPreviousWeek = today.AddDays(-daysSinceSunday - 7);
+			DateTime lastSaturday = lastSundayOfPreviousWeek.AddDays(6);
+			GlobalSettings.FechaInicio = lastSundayOfPreviousWeek.ToString("dd/MM/yyyy");
+			GlobalSettings.FechaFin = lastSaturday.ToString("dd/MM/yyyy");
+			CultureInfo ci = CultureInfo.CurrentCulture;
+			int weekOfYear = ci.Calendar.GetWeekOfYear(lastSaturday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+			GlobalSettings.NumSemana = weekOfYear.ToString();
+		}
+	}
+}
---
+++ CONFIG/VerifGeckodriver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROBOTPRUEBA_V1.CONFIG
+{
+	internal class VerifGeckodriver
+	{
+		public static void fingecko()
+		{
+			var procesos = Process.GetProcessesByName("geckodriver");
+			foreach (var proceso in procesos)
+			{
+				proceso.Kill(); // Termina el proceso
+			}
+		}
+	}
+}
